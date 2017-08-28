a521d9b4c1dcad06112bf7ae7506210d91cabcec
DanielBabcock <babcock.daniel.b@gmail.com>
2017-08-28 17:13:26 -0500

    start over

--- array-methods.js
+++ array-methods.js
@@ -1,18 +1,15 @@
-// *****************CHAINING METHODS EXERCISE*****************
-/*instructions
-Sort the numbers in DESCENDING order (10, 9, 8, 7, etc).
-Remove any integers greater than 19.
-Multiply each remaining number by 1.5 and then substract 1.
-Then output (console) the sum of all the resulting numbers.
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+/*
+ Use the forEach method to add the name of each planet
+ to a div element in your HTML with an id of "planets".
 */
-// var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-// let endGame = integers
+var el = document.getElementById("planets");
 
-// .sort(function(a, b) {return b-a})
-// .filter((num) => {return num < 19})
-// .map((num) => {return(1.5 * num) - 1})
-// .reduce((previous, current) => {return previous + current});  
+// Use the map method to create a new array where the first letter of each planet is capitalized
 
-// console.log("endGame: ", endGame);
+// Use the filter method to create a new array that contains planets with the letter 'e'
 
-var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];let endGame = integers.sort(function(a, b) {return b-a}).filter((num) => {return num < 19}).map((num) => {return(1.5 * num) - 1}).reduce((previous, current) => {return previous + current}); console.log("endGame: ", endGame);
+// Use the reduce method to create a sentence from the words in the following array
+
+var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
